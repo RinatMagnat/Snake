4df1c848849e1a18177d0010955f68dfad966135
RinatMagnat <50412433+RinatMagnat@users.noreply.github.com>
2020-11-01 16:22:10 +0500

    2  create Map and Move cube

--- Snake_Game/Snake_Game/Form1.Designer.cs
+++ Snake_Game/Snake_Game/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.hadSnake = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.hadSnake)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hadSnake
+            // 
+            this.hadSnake.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.hadSnake.Location = new System.Drawing.Point(0, 0);
+            this.hadSnake.Name = "hadSnake";
+            this.hadSnake.Size = new System.Drawing.Size(40, 40);
+            this.hadSnake.TabIndex = 0;
+            this.hadSnake.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(603, 495);
+            this.Controls.Add(this.hadSnake);
+            this.Name = "Form1";
+            this.Text = "Snake";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.hadSnake)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox hadSnake;
     }
 }
 
--- Snake_Game/Snake_Game/Form1.cs
+++ Snake_Game/Snake_Game/Form1.cs
@@ -12,9 +12,42 @@ namespace Snake_Game
 {
     public partial class Form1 : Form
     {
+        private int heigth = 800;
+        private int width = 800;
+        private int sizeOfSnake = 40;
         public Form1()
         {
             InitializeComponent();
+            this.KeyDown += new KeyEventHandler(KeyCath);
+            for (int i = 0; i < width / sizeOfSnake; i++) {
+              _generateMap(new Point(0,sizeOfSnake * i),new Size(width,1));
+                _generateMap(new Point(sizeOfSnake * i, 0), new Size(1, heigth));
+             
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _generateMap(Point p, Size s) {
+
+            PictureBox pb = new PictureBox();
+            pb.Location = p;
+            pb.BackColor = Color.Black;
+            pb.Size = s;
+            this.Controls.Add(pb);
+        }     
+        private void KeyCath(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode) {
+                case Keys.Left:  hadSnake.Location = new Point(hadSnake.Location.X - 40, hadSnake.Location.Y);   break;
+                case Keys.Right: hadSnake.Location = new Point(hadSnake.Location.X + 40, hadSnake.Location.Y);   break;
+                case Keys.Up: hadSnake.Location = new Point(hadSnake.Location.X, hadSnake.Location.Y - 40);  break;
+                case Keys.Down: hadSnake.Location = new Point(hadSnake.Location.X, hadSnake.Location.Y + 40);   break;
+                default:     break;
+            }
         }
     }
 }
